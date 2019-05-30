b10e5fe7af7804f3580ce7ecb0e36bf6edb45bd1
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-30 18:09:01 +0000

    Created recursive function to sum all number from M to N

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN)
+  {
+    return toN;
+  }
+  else
+  {
+    return fromN + sum(fromN + 1, toN);
+  }
 }
 
 module.exports = sum;
